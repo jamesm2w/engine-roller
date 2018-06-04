45516692b3de54bbe9ef7d96debc712996e64c28
Glitch (adventurous-market) <none>
2018-06-04 07:36:35 +0000

    🎯👰 Checkpoint ./rollEngine.js:329397/811

---
+++ rollEngine.js
@@ -0,0 +1,30 @@
+exports = module.exports = {};
+
+var elDiv = (ar1, num) => {
+  let res = new Array(ar1.length);
+  for (let i = 0; i < ar1.length; i++) {
+    res[i] = ar1[i] / num;
+  }
+  return res;
+}
+
+var elMult = (ar1, num) => {
+  let res = new Array(ar1.length);
+  for (let i = 0; i < ar1.length; i++) {
+    res[i] = ar1[i] * num;
+  }
+  return res;
+}
+
+var arrayMult = (ar1, ar2, pwr, max, min) => {
+  if (ar1.length != ar2.length) {
+    throw "Arrays are not same length. Can't do this";
+  }
+  for (let i = 0; i < ar1.length; i++) {
+    ar1[i] = Math.pow(ar2[i], pwr * (max-min)) + min;
+  }
+}
+
+exports.rollEngine = (n, min, max, total) => {
+  e
+};
