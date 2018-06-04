943d0ac3eb2c7349ff95091f7b314e20ea288dbc
Glitch (engine-roller) <none>
2018-06-04 10:53:35 +0000

    🌗🍩 Checkpoint ./public/client.js:329397/1057 ./views/index.html:329397/255

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-var elDiv = (ar1, num) => {
+var elDiv =  function (ar1, num) {
   let res = new Array(ar1.length);
   for (let i = 0; i < ar1.length; i++) {
     res[i] = ar1[i] / num;
@@ -6,7 +6,7 @@ var elDiv = (ar1, num) => {
   return res;
 }
 
-var elMult = (ar1, num) => {
+var elMult = function (ar1, num) {
   let res = new Array(ar1.length);
   for (let i = 0; i < ar1.length; i++) {
     res[i] = ar1[i] * num;
@@ -14,7 +14,7 @@ var elMult = (ar1, num) => {
   return res;
 }
 
-var arrayMult = (ar1, ar2, pwr, max, min) => {
+var arrayMult = function (ar1, ar2, pwr, max, min) {
   if (ar1.length != ar2.length) {
     throw "Arrays are not same length. Can't do this";
   }
@@ -24,24 +24,45 @@ var arrayMult = (ar1, ar2, pwr, max, min) => {
   return ar1;
 }
 
-var rollSchematic = (min, max, total) => {
+var rollSchematic = function (min, max, total) {
   let engine = [max + 1, max + 1, max + 1, max + 1, max + 1];
   let rollMax = -1, rollMin = 0;
-  console.log(engine)
   while (engine[0] >= max || engine[1] >= max || engine[2] >= max || engine[3] >= max || engine[4] >= max) {
     rollMax++;
     engine = arrayMult(engine, [random.real(1,5),random.real(1,5),random.real(1,5),random.real(1,5),random.real(1,5)], 1, max, min)
-    console.log(engine)
     engine = elMult(elDiv(engine, eval(engine.join('+'))), total);
-    console.log(engine)
     
     while (engine[0] < min || engine[1] < min || engine[2] < min || engine[3] < min || engine[4] < min) {
       rollMin++;
       engine = arrayMult(engine, [random.real(1,5),random.real(1,5),random.real(1,5),random.real(1,5),random.real(1,5)], 1, max, min)
-      console.log(engine)
       engine = elMult(elDiv(engine, eval(engine.join('+'))), total);
-      console.log(engine)
     }
   }
   return engine;
 };
+
+var rollEngine = function (tier) {
+  var max = 180
+  switch (tier) {
+    case 6:
+      max = 300
+    case 5:
+      max = 235
+    case 4:
+      max = 180
+    case 3: 
+      max = 135
+    case 2:
+      max = 100
+    case 1:
+      max = 75
+    case 7:
+      max = 375
+    default:
+      max = 100
+              }
+  var result = rollSchematic(5, 100, max);
+  for (var i = 0; i < result.length; i++){
+    docume
+  }
+}
