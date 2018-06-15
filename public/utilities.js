94b23f55fac1fe542ad52c7cee91a4c8bc972b93
Glitch (engine-roller) <none>
2018-06-15 17:35:38 +0000

    ✂️🌱 Checkpoint ./public/style.css:329397/1896 ./views/index.html:329397/4106 ./public/utilities.js:329397/1001

---
+++ public/utilities.js
@@ -0,0 +1,25 @@
+var showModal = function (modalID) {
+  document.getElementById(modalID + "Modal").style.display = "block";
+}
+
+var closeModal = function (e) {
+  var modals = document.getElementsByClassName("modal");
+  for (var modal of modals) {
+    if (modal.style.display == "block") {
+      modal.style.display = "none";
+    }
+  }
+}
+
+var handleWindowClick = function (e) {
+  if (event.target.classList.contains("modal")) {
+      closeModal(e);
+  }
+}
+
+
+
+var closeBtns = document.getElementsByClassName("modalClose");
+for (var element of closeBtns) {
+  element.addEventListener("click", closeModal);
+}
