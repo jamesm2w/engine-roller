6172746b79abbccb16f91f925c9ff6453d5a475a
Glitch (engine-roller) <none>
2018-06-16 07:28:11 +0000

    🎂🌒 Checkpoint ./public/eventHandler.js:329397/763 ./public/utilities.js:329397/637 ./public/config/engineConfig.js:329397/1433 ./public/client.js:329397/737

--- public/eventHandler.js
+++ public/eventHandler.js
@@ -1,87 +1,88 @@
 (function(window) {
-var EventHandler = (function(window) {
-  
-  var EventHandler = function(options) {};
+  var EventHandler = (function(window) {
 
-  EventHandler.prototype.handleWindowClick = function (e) {
-    if (event.target.classList.contains("modal")) {
-      window.Utilities.closeOpenModal(e);
-    }
-  };
+    var EventHandler = function(options) {};
 
-  EventHandler.prototype.handleLoadAction = function (e) {
-    var obj = JSON.parse(window.localStorage.getItem(e.target.parentElement.id));
-    var engine = window.Schematic.parseJson(obj);
-    engine.displayEngine();
-    window.Utilities.closeOpenModal();
-  };
+    EventHandler.prototype.handleWindowClick = function (e) {
+      if (event.target.classList.contains("modal")) {
+        window.Utilities.closeOpenModal(e);
+      }
+    };
 
-  EventHandler.prototype.handleForgetAction = function (e) {
-    var name = e.target.parentElement.id;
-    var confirmation = confirm("Are you sure you want to remove this engine? It will be lost forever.");
-    if (confirmation) {
-      window.localStorage.removeItem(name);
+    EventHandler.prototype.handleLoadAction = function (e) {
+      var obj = JSON.parse(window.localStorage.getItem(e.target.parentElement.id));
+      var engine = window.Schematic.parseJson(obj);
+      engine.displayEngine();
       window.Utilities.closeOpenModal();
-      return true;
+    };
+
+    EventHandler.prototype.handleForgetAction = function (e) {
+      var name = e.target.parentElement.id;
+      var confirmation = confirm("Are you sure you want to remove this engine? It will be lost forever.");
+      if (confirmation) {
+        window.localStorage.removeItem(name);
+        window.Utilities.closeOpenModal();
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    EventHandler.prototype.handleEngineSaveClick = function (e) {
+      if (window.loadedEngine != undefined) {
+        window.loadedEngine.saveSchematic();
+        e.target.innerHTML = "(Saved Schematic)";
+        e.target.style.color =  "green";
+      } else {
+        alert("Tried to save an undefined engine");
+      }
+    };
+
+    EventHandler.prototype.handleValueChange = function (e) {
+      var value = e.target.value;
+      if ( value > 99) {
+        e.target.value = 99;
+      } else if ( value < 5) {
+        e.target.value = 5;
+      }
+    };
+
+    EventHandler.prototype.handleTierChange = function (e) {
+      var value = e.target.value;
+      if (value > 8) {
+        e.target.value = 8;
+      } else if (value < 1) {
+        e.target.value = 1;
+      }
+    };
+
+    EventHandler.prototype.handleQualifierClick = function (e) {
+      var current = e.target.value;
+      e.target.value = EventHandler.prototype.incrementQualifier(current);
+    };
+
+    EventHandler.prototype.handleMatMouseEnter = function (e) {
+      var el = e.target, 
+          name = el.querySelector(".wa-header").innerHTML, 
+          effects = window.loadedEngine.config.statEffects[name];
+      for (var i = 0; i < effects.length; i++) {
+        document.getElementById("schem-stat-" + effects[i]).style.backgroundColor = "#8bc34a";
+      }
+    };
+
+    EventHandler.prototype.handleMatMouseLeave = function (e) {
+      for (var i = 0; i < 5; i++) {
+        document.getElementById("schem-stat-" + i).style.backgroundColor = "#668ec3";
+      }
+    };
+    
+    return EventHandler;
+  })(window);
+
+    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
+      module.exports = EventHandler;
     } else {
-      return false;
+      window.EventHandler = EventHandler;
     }
-  };
 
-  EventHandler.prototype.handleEngineSaveClick = function (e) {
-    if (window.loadedEngine != undefined) {
-      window.loadedEngine.saveSchematic();
-      e.target.innerHTML = "(Saved Schematic)";
-      e.target.style.color =  "green";
-    } else {
-      alert("Tried to save an undefined engine");
-    }
-  };
-  
-  EventHandler.prototype.handleValueChange = function (e) {
-    var value = e.target.value;
-    if ( value > 99) {
-      e.target.value = 99;
-    } else if ( value < 5) {
-      e.target.value = 5;
-    }
-  };
-
-  EventHandler.prototype.handleTierChange = function (e) {
-    var value = e.target.value;
-    if (value > 8) {
-      e.target.value = 8;
-    } else if (value < 1) {
-      e.target.value = 1;
-    }
-  };
-  
-  EventHandler.prototype.handleQualifierClick = function (e) {
-    var current = e.target.value;
-    e.target.value = EventHandler.prototype.incrementQualifier(current);
-  };
-
-  EventHandler.prototype.handleMatMouseEnter = function (e) {
-    var el = e.target, 
-        name = el.querySelector(".wa-header").innerHTML, 
-        effects = window.loadedEngine.config.statEffects[name];
-    for (var i = 0; i < effects.length; i++) {
-      document.getElementById("schem-stat-" + effects[i]).style.backgroundColor = "#8bc34a";
-    }
-  };
-
-  EventHandler.prototype.handleMatMouseLeave = function (e) {
-    for (var i = 0; i < 5; i++) {
-      document.getElementById("schem-stat-" + i).style.backgroundColor = "#668ec3";
-    }
-  };
-  
-})(window);
-                    
-  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
-    module.exports = EventHandler;
-  } else {
-    window.EventHandler = EventHandler;
-  }
-  
 })(window);
--- public/utilities.js
+++ public/utilities.js
@@ -1,75 +1,79 @@
 (function(window) {
-var Utilities = (function(window) {
-  var random = (typeof window.random == undefined) ? new require("random-js")() : window.random;
   
-  var Utilities = function (options) {};
-  
-  Utilities.prototype.elDiv = function (ar1, num) {
-    var res = new Array(ar1.length);
-    for (var i = 0; i < ar1.length; i++) {
-      res[i] = ar1[i] / num;
-    }
-    return res;
-  }
-  
-  Utilities.prototype.elMult = function (ar1, num) {
-    var res = new Array(ar1.length);
-    for (var i = 0; i < ar1.length; i++) {
-      res[i] = ar1[i] * num;
-    }
-    return res;
-  }
-  
-  Utilities.prototype.arrayMult = function (ar1, ar2, pwr, max, min) {
-    if (ar1.length != ar2.length) {
-      throw "Arrays are not same length. Can't do this";
-    }
-    for (let i = 0; i < ar1.length; i++) {
-      ar1[i] = Math.pow(ar2[i], pwr) * (max-min) + min;
-    }
-    return ar1;
-  }
-  
-  Utilities.prototype.randomStatArray = function (n) {
-    var a = new Array(n);
-    for (var i = 0; i < n; i++) {
-      a[i] = [Utilities.random.real(1,5)];
-    }
-    return a;
-  }
-  
-  Utilities.prototype.showModal = function (modalID) {
-    document.getElementById(modalID + "Modal").style.display = "block";
+  var Utilities = (function(window) {
+    var random = (typeof window.random == undefined) ? new require("random-js")() : window.random;
 
-    if (modalID = "LoadEngine") {
-      document.getElementById("LoadEngineModal").getElementsByClassName("modal-body")[0].innerHTML = "";
-      window.UI.buildLoaderUI();
+    var Utilities = function (options) {};
+
+    Utilities.prototype.elDiv = function (ar1, num) {
+      var res = new Array(ar1.length);
+      for (var i = 0; i < ar1.length; i++) {
+        res[i] = ar1[i] / num;
+      }
+      return res;
     }
-  }
-  
-  Utilities.prototype.closeOpenModal = function (e) {
-    var modals = document.getElementsByClassName("modal");
-    for (var modal of modals) {
-      if (modal.style.display == "block") {
-        modal.style.display = "none";
+
+    Utilities.prototype.elMult = function (ar1, num) {
+      var res = new Array(ar1.length);
+      for (var i = 0; i < ar1.length; i++) {
+        res[i] = ar1[i] * num;
+      }
+      return res;
+    }
+
+    Utilities.prototype.arrayMult = function (ar1, ar2, pwr, max, min) {
+      if (ar1.length != ar2.length) {
+        throw "Arrays are not same length. Can't do this";
+      }
+      for (let i = 0; i < ar1.length; i++) {
+        ar1[i] = Math.pow(ar2[i], pwr) * (max-min) + min;
+      }
+      return ar1;
+    }
+
+    Utilities.prototype.randomStatArray = function (n) {
+      var a = new Array(n);
+      for (var i = 0; i < n; i++) {
+        a[i] = [Utilities.random.real(1,5)];
+      }
+      return a;
+    }
+
+    Utilities.prototype.showModal = function (modalID) {
+      document.getElementById(modalID + "Modal").style.display = "block";
+
+      if (modalID = "LoadEngine") {
+        document.getElementById("LoadEngineModal").getElementsByClassName("modal-body")[0].innerHTML = "";
+        window.UI.buildLoaderUI();
       }
     }
-  }
-  
-  Utilities.prototype.equals = function (el) {
-    return el == this;
-  }
-  
-  Utilities.prototype.incrementQualifier = function (current) {           //Maybe I'll flesh this out later. Likely not, since handling safety checks for <, = would be a pain.
-    var qualifiers = [">", "<", "=", ">=", "<="];                         //implementing >= and <= wouldn't be too difficult, but they are pretty lesser, and the value input
-    let a = qualifiers.findIndex(Utilities.prototype.equals, current) + 1;//could esily be changed to mimic logic similar. e.g. "> 49" is the same as ">= 50"
-    if (a >= qualifiers.length) {
-      var next = qualifiers[0];
-    } else {
-      var next = qualifiers[a];
+
+    Utilities.prototype.closeOpenModal = function (e) {
+      var modals = document.getElementsByClassName("modal");
+      for (var modal of modals) {
+        if (modal.style.display == "block") {
+          modal.style.display = "none";
+        }
+      }
     }
-    return ">";
-  }
+
+    Utilities.prototype.equals = function (el) {
+      return el == this;
+    }
+
+    Utilities.prototype.incrementQualifier = function (current) {           //Maybe I'll flesh this out later. Likely not, since handling safety checks for <, = would be a pain.
+      var qualifiers = [">", "<", "=", ">=", "<="];                         //implementing >= and <= wouldn't be too difficult, but they are pretty lesser, and the value input
+      let a = qualifiers.findIndex(Utilities.prototype.equals, current) + 1;//could esily be changed to mimic logic similar. e.g. "> 49" is the same as ">= 50"
+      if (a >= qualifiers.length) {
+        var next = qualifiers[0];
+      } else {
+        var next = qualifiers[a];
+      }
+      return ">";
+    }
+
+    return Utilities;
+  
   })(window);
   
   if (typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
