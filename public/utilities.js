f2632e57eb9fcec8582940f29bd549fd396b0f43
Glitch (engine-roller) <none>
2018-09-02 17:24:28 +0000

    📫🐬 Checkpoint ./public/utilities.js:329397/722 ./views/index.html:329397/172

--- public/utilities.js
+++ public/utilities.js
@@ -35,37 +35,10 @@
       return false;
     }
 
-
-    Utilities.prototype.elDiv = function (ar1, num) {
-      var res = new Array(ar1.length);
-      for (var i = 0; i < ar1.length; i++) {
-        res[i] = ar1[i] / num;
-      }
-      return res;
-    }
-
-    Utilities.prototype.elMult = function (ar1, num) {
-      var res = new Array(ar1.length);
-      for (var i = 0; i < ar1.length; i++) {
-        res[i] = ar1[i] * num;
-      }
-      return res;
-    }
-
-    Utilities.prototype.arrayMult = function (ar1, ar2, pwr, max, min) {
-      if (ar1.length != ar2.length) {
-        throw "Arrays are not same length. Can't do this";
-      }
-      for (let i = 0; i < ar1.length; i++) {
-        ar1[i] = Math.pow(ar2[i], pwr) * (max-min) + min;
-      }
-      return ar1;
-    }
-
     Utilities.prototype.randomStatArray = function (n) {
       var a = new Array(n);
       for (var i = 0; i < n; i++) {
-        a[i] = [window.random.real(1,5)];
+        a[i] = window.random.real(1,5);
       }
       return a;
     }
